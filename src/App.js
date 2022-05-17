974730e39ecc04b7b4c968ea18b49251502cbdc0
ulancho <ulan.five@gmail.com>
2022-05-17 21:16:41 +0600

    feat: added fetching

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React from "react";
-import {BrowserRouter, Routes, Route, NavLink} from "react-router-dom";
-import {Main, About, Products, Contacts, Post, NotFound, Posts} from "./Pages";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Main from "./pages/Main/Main";
 import "./App.css";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <nav className="nav">
-                <ul>
-                    <li>
-                        <NavLink className={(object) => object.isActive ? 'active-link' : ''} to="/">Главная</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={(object) => object.isActive ? 'active-link' : ''} to="/products">Продукты</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={(object) => object.isActive ? 'active-link' : ''} to="/about">О нас</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={(object) => object.isActive ? 'active-link' : ''} to="/contacts">Контакты</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={(object) => object.isActive ? 'active-link' : ''} to="/posts">Посты</NavLink>
-                    </li>
-                </ul>
-            </nav>
             <Routes>
                 <Route path="/" element={<Main/>}/>
-                <Route path="/products" element={<Products/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/contacts" element={<Contacts/>}/>
-                <Route path="/posts" element={<Posts/>}/>
-                <Route path="/post/:id" element={<Post/>}/>
-                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,21 +1,38 @@
-import React from "react";
-import Header from "../../components/header/Header";
-import BlogEntries from "../../components/BlogEntries/BlogEntries";
-import IntroductionMenu from "../../components/IntroductionMenu/IntroductionMenu";
-import Footer from "../../components/Footer/Footer";
+import React, {useEffect, useState} from "react";
+import styles from "./Main.module.css";
 
 const Main = () => {
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        const url = 'https://jsonplaceholder.typicode.com/users';
+
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setUserData(data)
+            })
+    },[]);
+
+
     return (
-        <>
-            <div className="w3-content" style={{maxWidth: "1400px"}}>
-                <Header/>
-                <div className="w3-row">
-                    <BlogEntries/>
-                    <IntroductionMenu/>
-                </div>
-            </div>
-            <Footer/>
-        </>
+        <div className={styles.container}>
+            {
+                userData.map((item, index) => {
+                    return (
+                        <div key={index} className={styles.card}>
+                            <p>{item.name}</p>
+                            <p>{item.username}</p>
+                            <p>{item.email}</p>
+                            <p>{item.phone}</p>
+                            <p>{item.company.name}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
 
